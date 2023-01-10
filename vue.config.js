b5e53964f00cd6c326e4ec44889447d0c9bb2d56
Latrevion <xiaopikaqiu@outlook.com>
2023-01-10 16:07:17 +0800

    修复scss 引入错误

--- vue.config.js
+++ vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
       //   }
       // },
       scss:{
-        additionalData: `@import "./styles/main.scss";`,
+        additionalData: `@import "./src/styles/main.scss";`,
       }
     },
   },
