9f725fcb0d1d45b2e4744cd25adad34929928c79
Latrevion <xiaopikaqiu@outlook.com>
2023-03-20 20:00:58 +0800

    语言包设置

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <template>
-<router-view></router-view>
+  <el-config-provider :locale="locale">
+    <router-view></router-view>
+  </el-config-provider>
 </template>
 
 <style lang="scss">
 #app {
 }
 </style>
+<script>
+import {defineComponent} from "vue"
+import {ElConfigProvider} from "element-plus"
+
+import zhCn from "element-plus/dist/locale/zh-cn.mjs"
+
+export default defineComponent({
+  components: {
+    ElConfigProvider,
+  },
+  setup() {
+    return {
+      locale: zhCn,
+    }
+  },
+})
+</script>
