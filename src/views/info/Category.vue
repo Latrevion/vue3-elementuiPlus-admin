93a2f32fecab24c11c36954020184171d24ad6f0
Latrevion <xiaopikaqiu@outlook.com>
2023-03-20 21:08:31 +0800

    渲染树形组件

--- src/views/info/Category.vue
+++ src/views/info/Category.vue
@@ -2,17 +2,51 @@
   <el-button type="danger">添加一级分类</el-button>
   <hr class="spacing-hr">
   <el-row>
-    <el-col :span="6">树形菜单</el-col>
+    <el-col :span="6">
+      <el-tree
+        :data="data.tree_data"
+        :props="data.defaultProps"
+        @node-click="handlerNodeClick"
+        default-expand-all
+      ></el-tree>
+    </el-col>
     <el-col :span="18">输入框</el-col>
   </el-row>
 </template>
 
 <script>
+import {reactive} from "vue"
+
 export default {
-  setup() {
+  setup(props) {
+    const data =reactive({
+      tree_data:[
+        {
+          text:'一级 1'
+          ,
+          children:[
+            {
+              text:'二级 1-1',
+              children:[
+                {
+                  text:'三级 1-1-1'
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      defaultProps:{
+        children:'children',
+        label:'text'
+      }
+    })
 
+    const handlerNodeClick= ( )=>{
 
-    return {}
+    }
+
+    return {data,handlerNodeClick}
   }
 }
 </script>
